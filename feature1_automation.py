b16a9ebeabd7de26d8474650c50a70cc7dee9034
iroda9900 <ishukurova0311@gmail.com>
2023-01-10 13:16:04 -0500

    feature1 automation file

---
+++ feature1_automation.py
@@ -0,0 +1,11 @@
+import  time
+
+print("automation feature1 started")
+print("Scenario1 is working")
+time.sleep(1)
+print("Scenario2 is working")
+time.sleep(1)
+print("Scenario3 is working")
+time.sleep(1)
+print("Scenario4 is working")
+print("automation feature1 completed")
